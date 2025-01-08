ed4804782c36d5ded9847a1676e4c801d85e3788
causeUrGnocchi <gkussunoki@gmail.com>
2025-01-08 20:28:50 -0300

    add solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,33 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
   // write code here
+  return actions
+    .reduce(
+      (history, action) => {
+        switch (action.type) {
+          case 'clear':
+            return history.concat({});
+          case 'addProperties':
+            return history.concat({
+              ...history.at(-1),
+              ...action.extraData,
+            });
+          case 'removeProperties':
+            const aux = { ...history.at(-1) };
+
+            for (const key of action.keysToRemove) {
+              delete aux[key];
+            }
+
+            return history.concat(aux);
+        }
+      },
+      [state],
+    )
+    .slice(1);
 }
 
 module.exports = transformStateWithClones;
